8a627323c0773cc07e33a1103da87f9efc276e08
Malte Swart <malte.swart@student.hpi.uni-potsdam.de>
2011-01-17 18:46:00 +0100

    plugins: ConnectionPlugin now use EasyPlugin

--- backend/plugins/network.py
+++ backend/plugins/network.py
@@ -1,47 +1,37 @@
 import Plugin
 import connection
 
-class ConnectionPlugin(Plugin.Plugin):
+class ConnectionPlugin(Plugin.EasyPlugin):
 	'''The class implemented the network management and routing for pycc
 	'''
-	#names under which the plugins wants to be registered
-	registeredCommands=['getnodeid','connectto','relay','status']
-	priority=0
+	def init(self):
+		self.server = self.manager.server
 
-	#Any command send from the PyCCManager will be here
-	def recvCommand(self, package):
-		'''all commands for this plugin are passed to this function
+	def commandR_getNodeId(self, package):
+		''' return the node id of the current backend'''
+		return self.server.getNodeId()
 
-con is of type backend.connection.PyCCPackage
-'''
-		if package.type != connection.PyCCPackage.TYPE_REQUEST:
-			return
-		if package.command == 'getNodeId':
-			package.data=self.PyCCManager.server.getNodeId()
-			package.connection.sendResponse(package)
-		elif package.command == 'status':
-			package.data=self.PyCCManager.server.status()
-			package.connection.sendResponse(package)
-		elif package.command.startswith('connectto'):
-			args=package.command.split(' ')
-			if len(args)==2:
-				args.append(62533)
-			if len(args)!=3:
-				return None
-			else:
-				self.PyCCManager.server.openConnection(args[1],int(args[2]))
-		elif package.command.startswith('relay'):
-			args=package.command.split(' ')
-			if len(args)!=2:
-				return None
-			else:
-				for con in self.PyCCManager.server.getConnectionList(args[1]):
-					data = package.data.decode('utf8').split('\n')
-					newPackage = connection.PyCCPackage()
-					newPackage.command = data[0]
-					newPackage.data = "\n".join(data[1:])
-					con.sendRequest(newPackage,callback = self.handleResponse,
-						callbackExtraArg = package)
+	def commandR_status(self, package):
+		return self.server.status()
+
+	def commandA_connectTo(self, package, host, port=62533):
+		if self.backend.openConnection(host,int(port)):
+			return True
+		else:
+			return False
+
+	def commandA_relay(self, package, node): # command with argument parsing
+		args=package.command.split(' ')
+		if len(args)!=2:
+			return None
+		else:
+			for con in self.backend.getNodeConnections(args[1]):
+				data = package.data.decode('utf8').split('\n')
+				newPackage = connection.PyCCPackage()
+				newPackage.command = data[0]
+				newPackage.data = "\n".join(data[1:])
+				con.sendRequest(newPackage,callback = self.handleResponse,
+					callbackExtraArg = package)
 
 	def handleResponse(self, newPackage, orgPackage = None):
 		newPackage.handle = orgPackage.handle
