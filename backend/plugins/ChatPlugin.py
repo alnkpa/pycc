3ebb0b19acc941ca3945c0ac33ea372fa21ed28c
Patrick Schmidt <bigpnator@gmail.com>
2011-01-30 14:35:15 +0100

    Documented ChatPlugin, so that it may be better understood

--- backend/plugins/ChatPlugin.py
+++ backend/plugins/ChatPlugin.py
@@ -7,16 +7,25 @@ class ChatPlugin(Plugin.EasyPlugin):
 		use recvMessage to send a message to the user of this plugin
 		'''
 
-	def commandAU_sendMessage(self, package,  argument):
-		name = argument
+	def commandAU_sendMessage(self, package, name):
+		'''A command called when a message should be send to a user
+			command is structured like the following: "sendMessage <username>"
+			message to be send should be in package.data
+			'''
+		# package.data should be the message
 		message = package.data
 		try:
+			# search for the running contactPlugin
 			contactPlugin = self.PyCCManager.searchPlugin("ContactPlugin")
 			try:
+				# search for the relevant nodeId
 				accountNodeId = contactPlugin.returnNodeId(name)
+				# change the command so the other backend will know it belongs to it
 				package.command = "recvMessage"
+				# send the message to every connection belonging to the nodeId
 				for connection in self.backend.getNodeConnections(accountNodeId):
 					connection.sendRequest(package)
+			# just some error handling
 			except ValueError:
 				package.type = "E"
 				package.data = "No such accountname"
@@ -27,14 +36,21 @@ class ChatPlugin(Plugin.EasyPlugin):
 			package.connection.sendError(package)
 	
 	def command_recvMessage(self, package):
+		'''A command called when a new message arrives
+			message to be received is in package.data
+			'''
 		nodeId = package.connection.partnerNodeId
 		try:
+			# search for the running contactPlugin
 			contactPlugin = self.PyCCManager.searchPlugin("ContactPlugin")
 			try:
+				# search for the relevant username
 				accountName = contactPlugin.returnUserName(nodeId)
+				# forward message to every frontend
 				for connection in self.backend.getNodeConnections(":frontend"):
 					package.command = "newMessage " + accountName
 					connection.sendRequest(package)
+			# error handling
 			except ValueError:
 				package.type = "E"
 				package.data = "No such nodeId"
